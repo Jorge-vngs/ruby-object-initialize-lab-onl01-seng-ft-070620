29d9e3475320721dc7710e153fdfa80c4e21de89
Jorge Venegas <brangelina01@gmail.com>
2020-07-18 04:45:37 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,12 @@
 class Dog
-  def initialize (dogs_name, dogs_breed = "Mutt")
-    @name = dogs_name
-    @breed = dogs_breed
-  end
-
-  def name=(dogs_name, dogs_breed = "Mutt")
-    @name = dogs_name
-    @breed = dogs_breed
-  end
+    def initialize (dogs_name, dogs_breed = "Mutt")
+       @name = dogs_name
+       @breed = dogs_breed
+end
+    def name=(dogs_name, dogs_breed = "Mutt")
+      @name = dogs_name
+      @breed = dogs_breed
+end
 
   def name
     @name
